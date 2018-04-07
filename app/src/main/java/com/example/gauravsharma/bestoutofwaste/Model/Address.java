778dbe85d31cdcfc1d64727c6222c1fa879bf98f
ankur <ankur.yadav349@gmail.com>
2018-04-07 16:44:13 +0530

    Adding user class

---
+++ app/src/main/java/com/example/gauravsharma/bestoutofwaste/Model/Address.java
@@ -0,0 +1,18 @@
+package com.example.gauravsharma.bestoutofwaste.Model;
+public class Address {
+    String userID;
+    String longitude;
+    String latitude;
+    String streetAddress1;
+    String streetAddress2;
+    String landMark;
+    String city;
+    String state;
+    String pinCode;
+    String country;
+
+    Address() {
+
+
+    }
+}
---
+++ app/src/main/java/com/example/gauravsharma/bestoutofwaste/Model/User.java
@@ -0,0 +1,23 @@
+package com.example.gauravsharma.bestoutofwaste.Model;
+
+import java.util.Date;
+public class User {
+    public String userID;
+    public String firstName;
+    public String lastName;
+    public String emailAddress;
+    public String gender;
+    public Date dateOfBirth;
+    public String phoneNumber;
+    public Address address;
+    public Address pickUpAddress;
+    public boolean flag;
+    public boolean registered;
+    public int lastCount;
+    public  Date lastSeen;
+    public Date updateAccount;
+    public Date createAccount;
+    public User(){
+
+    }
+}
